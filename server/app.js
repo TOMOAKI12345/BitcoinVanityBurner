3a8c2f0d8125c619bdddc1bfe7ff9c218248ff47
TOMOAKI12345 <tomoaki12345@gmail.com>
2015-04-01 04:59:02 +0900

    login deleted

--- server/app.js
+++ server/app.js
@@ -30,9 +30,38 @@ var socketio = require('socket.io')(server, {
   serveClient: (config.env === 'production') ? false : true,
   path: '/socket.io-client'
 });
+var winston = require('winston'),
+  expressWinston = require('express-winston');
 require('./config/socketio')(socketio);
 require('./config/express')(app);
+
+// express-winston logger makes sense BEFORE the router.
+app.use(expressWinston.logger({
+  transports: [
+    new winston.transports.Console({
+      json: true,
+      colorize: true
+    })
+  ]
+}));
+
 require('./routes')(app);
+
+// express-winston errorLogger makes sense AFTER the router.
+app.use(expressWinston.errorLogger({
+  transports: [
+    new winston.transports.Console({
+      json: true,
+      colorize: true
+    })
+  ]
+}));
+
+// Optionally you can include your custom error handler after the logging.
+app.use(express.errorLogger({
+  dumpExceptions: true,
+  showStack: true
+}));
 
 // Start server
 server.listen(config.port, config.ip, function () {
